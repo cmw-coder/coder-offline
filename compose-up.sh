ef6dc018ea988e8907fbfe4f86085d91b14c4446
ParticleG <particle_g@outlook.com>
2025-08-12 03:34:48 +0800

    - Update sources

--- compose-up.sh
+++ compose-up.sh
@@ -9,4 +9,4 @@ DOCKER_COMPOSE_FILE_PATH="$1"
 export CODER_DERP_SERVER_REGION_ID="$(hostname -I | awk '{print $1}')"
 export CODER_DERP_SERVER_RELAY_URL="http://$(hostname -I | awk '{print $1}'):7080"
 export DOCKER_GROUP=$(getent group docker | cut -d: -f3)
-docker compose -f "${DOCKER_COMPOSE_FILE_PATH}" up -d
+docker compose -f "${DOCKER_COMPOSE_FILE_PATH}" up --build -d --force-recreate
